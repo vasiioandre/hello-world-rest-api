62a81196a02947ebfb864b4a19729189ec0c735f
vasiioandre <vasi_ioandre@yahoo.com>
2024-04-15 22:42:51 +0300

    Support for XML and Internalization

--- src/main/java/com/ioana/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/ioana/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.ioana.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	
 	//@RequestMapping(method=RequestMethod.GET, path="/hello-world")
 	@GetMapping(path="/hello-world")
@@ -21,6 +31,13 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path="/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		Object locale = LocaleContextHolder.getLocale();
+		
+		return messageSource.getMessage("good.morning.message", null, "Default Message", (Locale) locale);
 	}
 
 }
