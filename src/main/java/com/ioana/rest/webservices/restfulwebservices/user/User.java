be82f553f78a145d6a459f7183421da73d248450
vasiioandre <vasi_ioandre@yahoo.com>
2024-04-19 12:08:13 +0300

    Create Post entity with Many to One relationship with User entity

--- src/main/java/com/ioana/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/ioana/rest/webservices/restfulwebservices/user/User.java
@@ -1,12 +1,14 @@
 package com.ioana.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -23,6 +25,10 @@ public class User {
 	
 	@Past(message="Birth Date should be in the past")
 	private LocalDate birthDate;  
+	
+	@OneToMany(mappedBy="user")
+	@JsonIgnore
+	private List<Post> posts;
 	
 	protected User() {
 		
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,17 @@
 insert into user_details(id, birth_date, name)
-values (10001, current_date(), 'Ioana');
+values(10001, current_date(), 'Ioana');
 
 insert into user_details(id, birth_date, name)
-values (10002, current_date(), 'Rose');
+values(10002, current_date(), 'Rose');
 
 insert into user_details(id, birth_date, name)
-values (10003, current_date(), 'Jane');
+values(10003, current_date(), 'Jane');
+
+insert into post(id, description, user_id)
+values(20001, 'I want to learn Spring', 10001);
+
+insert into post(id, description, user_id)
+values(20002, 'I want to learn AWS', 10001);
+
+insert into post(id, description, user_id)
+values(20003, 'I want to learn Full Stack', 10002);
